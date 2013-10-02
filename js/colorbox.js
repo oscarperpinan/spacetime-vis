2be0f4cea49c8673a0f971e967271a08f2a91fcb
Oscar Perpiñán <oscar.perpinan@gmail.com>
2013-10-02 20:15:49 +0200

    New thumbnails; playing with colorbox instead of PDFs

---
+++ js/colorbox.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $("a#timeHorizontal").colorbox({rel:'horizontal', transition: "fade", width:"40%", height:"60%"});
+    $("a#timeConditional").colorbox({rel:'conditional', transition: "fade", width:"40%", height:"60%"});
+    $("a#timeComplementary").colorbox({rel:'complementary', transition: "fade", width:"40%", height:"60%"});
+    $(".svg").colorbox({iframe:true, width:"40%", height:"80%", scrolling:false});
+    // $(".group2").colorbox({rel:'group2', transition:"fade"});
+    // $(".group3").colorbox({rel:'group3', transition:"none", width:"75%", height:"75%"});
+    // $(".ajax").colorbox();
+    // $(".vimeo").colorbox({iframe:true, innerWidth:500, innerHeight:409});
+    // $(".iframe").colorbox({iframe:true, width:"80%", height:"80%"});
+    // $(".inline").colorbox({inline:true, width:"50%"});
+});
