13b7087c44ebf6505561b95d6fac888df540ab15
Oscar Perpiñán <oscar.perpinan@gmail.com>
2013-10-03 12:55:38 +0200

    Thumbnails fixed in spacetime.html

--- js/colorbox.js
+++ js/colorbox.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
     $(".outline-text-2 a").colorbox({width:"40%", height:"60%"});
     $(".svg").colorbox({iframe:true, width:"40%", height:"80%", scrolling:false});
-    // $(".vimeo").colorbox({iframe:true, innerWidth:500, innerHeight:409});
+    $(".vimeo").colorbox({iframe:true, innerWidth:600, innerHeight:600, scrolling:false});
 });
